4f3460ce68aefabaf9b39581513da8d218a99f2b
TanzilalGummilang <tanzilalgummilang@gmail.com>
2022-10-24 21:05:37 +0700

    fix update route

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StoreRouteRequest;
 use App\Http\Requests\UpdateRouteRequest;
 use App\Models\Route;
 use App\Models\Terminal;
+use Illuminate\Support\Facades\DB;
 
 class RouteController extends Controller
 {
@@ -17,19 +18,26 @@ class RouteController extends Controller
 
     public function store(StoreRouteRequest $request)
     {
-        $route = Route::create([
-            'code' => $request->code,
-            'origin' => $request->origin,
-            'destination' => $request->destination,
-            'time_taken' => $request->time_taken,
-            'bus_stop' => json_encode($request->bus_stop)
-        ]);
+        try {
+            DB::beginTransaction();
+            $route = Route::create([
+                'code' => $request->code,
+                'origin' => $request->origin,
+                'destination' => $request->destination,
+                'time_taken' => $request->time_taken,
+                'bus_stop' => json_encode($request->bus_stop)
+            ]);
 
-        self::jsonBusStop($route);
-        return response()->json([
-            'message' => "new route created successfully!",
-            'data' => $route
-        ]);
+            self::jsonBusStop($route);
+            DB::commit();
+            return response()->json([
+                'message' => "new route created successfully!",
+                'data' => $route
+            ]);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            // throw $th;
+        }
     }
 
     public function show(Route $route)
@@ -42,30 +50,48 @@ class RouteController extends Controller
 
     public function update(UpdateRouteRequest $request, Route $route)
     {
-        $method = $request->method();
-        if ($method == 'PATCH') {
-            $route->update($request->validated());
-        } else {
-            $route->update([
-                'code' => $request->code,
-                'origin' => $request->origin,
-                'destination' => $request->destination,
-                'time_taken' => $request->time_taken,
-                'bus_stop' => json_encode($request->bus_stop)
-            ]);
-        }
+        try {
+            DB::beginTransaction();
+            $method = $request->method();
+            if ($method == 'PATCH') {
+                if($request->bus_stop) $route->update(['bus_stop' => json_encode($request->bus_stop)]);
+                if($request->code) $route->update(['code' => $request->code]);
+                if($request->origin) $route->update(['origin' => $request->origin]);
+                if($request->destination) $route->update(['destination' => $request->destination]);
+                if($request->time_taken) $route->update(['time_taken' => $request->time_taken]);
+            } else {
+                $route->update([
+                    'code' => $request->code,
+                    'origin' => $request->origin,
+                    'destination' => $request->destination,
+                    'time_taken' => $request->time_taken,
+                    'bus_stop' => json_encode($request->bus_stop)
+                ]);
+            }
 
-        self::jsonBusStop($route);
-        return response()->json([
-            'message' => "route updated successfully!",
-            'data' => $route
-        ]);
+            self::jsonBusStop($route);
+            DB::commit();
+            return response()->json([
+                'message' => "route updated successfully!",
+                'data' => $route
+            ]);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            // throw $th;
+        }
     }
 
     public function destroy(Route $route)
     {
-        $route->delete();
-        return response()->json(['message' => "route delete successfully!"]);
+        try {
+            DB::beginTransaction();
+            $route->delete();
+            DB::commit();
+            return response()->json(['message' => "route delete successfully!"]);
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            // throw $th;
+        }
     }
 
     protected function jsonBusStop($route)
@@ -74,7 +100,7 @@ class RouteController extends Controller
         $terminals = Terminal::whereIn('id', array_column($route->bus_stop, 'id'))
             ->select('id', 'type', 'code', 'name', 'address')
             ->get();
-        $route->bus_stop = array_map(function($value) use ($terminals) {
+        $route->bus_stop = array_map(function ($value) use ($terminals) {
             $value['terminal'] = $terminals->where('id', $value['id'])->first();
             return $value;
         }, $route->bus_stop);
